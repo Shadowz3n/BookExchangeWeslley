2e66cf456e8037368c6a9a3679d0453849879cf2
root <emp.shad@gmail.com>
2016-12-21 16:52:10 -0200

    Menu

--- www/js/script.js
+++ www/js/script.js
@@ -1,37 +1,15 @@
 $(document).ready(function(){
-
-	document.addEventListener('touchstart', handleTouchStart, false);        
-	document.addEventListener('touchmove', handleTouchMove, false);
-
-	var xDown = null;                                                        
-	var yDown = null;                                                        
-
-	function handleTouchStart(evt){
-		xDown = evt.touches[0].clientX;
-		yDown = evt.touches[0].clientY;
-	};
-	
-	function handleTouchMove(evt){
-		if( ! xDown || ! yDown ) return;
-		var xUp = evt.touches[0].clientX;                                    
-		var yUp = evt.touches[0].clientY;
-		var xDiff = xDown - xUp;
-		var yDiff = yDown - yUp;
-		if(Math.abs(xDiff) > Math.abs(yDiff)){
-			return (xDiff>0)? 'left':'right';                     
-		}else{
-			return (yDiff>0)? 'up':'down';                                                              
-		}
-		xDown = null;
-		yDown = null;                                             
-	};
-
-
-	//document.addEventListener('deviceready', function(){
+	var touchContentX	= null, touchContentEndX	= null;
+	document.addEventListener('deviceready', function(){
 		$(document).on("click", ".menu, .menu_principal_bg", function(){
 			$(".menu").toggleClass("menu_ativo");
 			$(".menu_principal").toggleClass("menu_principal_ativo");
 			$(".menu_principal_bg").toggleClass("menu_principal_bg_ativo");
+		}).on("mousedown touchstart", "#content", function(e){
+			touchContentX	= e.pageX;
+		}).on("mouseup touchend", "#content", function(e){
+			touchContentEndX	= e.pageX;
+			if(touchContentX<Number(touchContentEndX-150)) $(".menu").click();
 		});
-	//}, false);
+	}, false);
 });
