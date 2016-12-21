ea843496734870ec44e840eaeb560a3a6295dce0
root <emp.shad@gmail.com>
2016-12-21 17:09:01 -0200

    Menu

--- www/js/script.js
+++ www/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	//document.addEventListener('deviceready', function(){
+	document.addEventListener('deviceready', function(){
 		$(document).on("click", ".menu, .menu_principal_bg", function(){
 			$(".menu").toggleClass("menu_ativo");
 			$(".menu_principal").toggleClass("menu_principal_ativo");
@@ -12,5 +12,5 @@ $(document).ready(function(){
 				if(touchContentX<Number(e.pageX-150)) $(".menu").click();
 			});
 		});
-	//}, false);
+	}, false);
 });
