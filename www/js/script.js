e58ea5279093cf4eceb2b8fe4ff61f6bd0f9d565
root <emp.shad@gmail.com>
2016-12-21 17:02:22 -0200

    Menu

--- www/js/script.js
+++ www/js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 	var touchContentX	= null;
-	document.addEventListener('deviceready', function(){
+	//document.addEventListener('deviceready', function(){
 		$(document).on("click", ".menu, .menu_principal_bg", function(){
 			$(".menu").toggleClass("menu_ativo");
 			$(".menu_principal").toggleClass("menu_principal_ativo");
 			$(".menu_principal_bg").toggleClass("menu_principal_bg_ativo");
+			setTimeout(function(){ $(".menu_principal_bg").toggleClass("menu_principal_bg_ativo_opacity"); }, 50);
 		}).on("mousedown touchstart", "#content", function(e){
 			touchContentX	= e.pageX;
 		}).on("mouseup touchend", "#content", function(e){
 			if(touchContentX<Number(e.pageX-150)) $(".menu").click();
 		});
-	}, false);
+	//}, false);
 });
